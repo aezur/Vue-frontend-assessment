d6fb4ac755165fcbfde91453ba36182bbd09a509
PeterMulligan <petermulligan88@gmail.com>
2026-02-14 05:09:48 +0100

    feat: add reusable list

---
+++ frontend/src/components/core/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot name="loading" v-if="loading" />
+    <slot name="error" v-else-if="error" :error="error" />
+    <slot name="empty" v-else-if="items.length === 0" />
+    <ul v-else>
+      <slot name="item" v-for="item in items" :key="item.id" :item="item" />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: { type: Array, required: true },
+  loading: Boolean,
+  error: String
+})
+</script>
